c79c56a69a9c35e873902d855a29f1a70b312585
Chris <clj@ollure.com>
2022-11-18 19:04:14 -0600

    working on timer

--- Pomodoro_Timer/src/Timer.js
+++ Pomodoro_Timer/src/Timer.js
@@ -3,7 +3,7 @@ import 'react-circular-progressbar/dist/styles.css';
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
 import SettingsButton from './SettingsButton';
-import {useContext} from 'react';
+import {useContext, useState, useEffect, useRef} from 'react';
 import SettingsContext from './SettingsContext';
 
 const red = '#f54e4e';
@@ -12,17 +12,71 @@ const green = '#4aec8c';
 function Timer() {
 	const settingsInfo = useContext(SettingsContext);
 
+	const [isPaused, setIsPaused] = useState(true);
+	const [mode, setMode] = useState('study');
+	const [secondsLeft, setSecondsLeft] = useState(0);
+	const secondsLeftRef = useRef(secondsLeft);
+	const isPausedRef = useRef(isPaused);
+	const modeRef = useRef(mode);
+
+
+	
+	
+	function switchMode() {
+		const nextMode = modeRef.current === 'study' ? 'break' : 'work';
+		const nextSeconds =(nextMode === 'study' ? settingsInfo.studyMinutes : settingsInfo.breakMinutes) * 60;
+
+		setMode(nextModeRef);
+		modeRef.current = nextMode;
+
+		setSecondsLeft(nextSeconds);
+		secondsLeftRef.c= nextSeconds;
+
+	function tick() {
+		secondsLeftRef.current = secondsLeftRef.current -1;
+		setSecondsLeft(secondsLeftRef.current);
+	}
+
+	function initTimer() {
+		setSecondsLeft(settingsInfo.studyMinutes * 60);
+	}
+
+	useEffect(() => {
+		initTimer();
+
+		const interval = setInterval(() => {
+			if (isPausedRef.current){
+				return;
+			}
+			if (secondsLeftRef.current === 0) {
+			 return	switchMode();
+			}
+			tick();
+		},1000);
+
+		return() => clearinterval(interval);
+	}, [settingsInfo]);
+
+	const totalSeconds = mode === 'study'
+	? settingsInfo.studyMinutes * 60 
+	: settingsInfo.breakMinutes * 60;
+	const percentage = Math.round(secondsLeft / totalSeconds * 100);
+	const minutes = Math.floor(secondsLeft / 60);
+	let seconds = secondsLeft % 60;
+	if(seconds < 10) seconds = '0' + seconds; 
+
 	return (
 		<div>
-			<CircularProgressbar value={60} text={'60%'} styles={buildStyles( {
+			<CircularProgressbar value={percentage} text={minutes + ':' + seconds} styles={buildStyles( {
 				textColor:"fff",
-				pathColor:red,
+				pathColor:mode === 'study' ? red : green,
 				tailColor:'rgba(255,255,255,.2)',
 			})} />
 			
 				<div style={{marginTop:'20px'}}>
-				<PlayButton />
-				<PauseButton />
+				{isPaused 
+					? <PlayButton onClick={() => { setIsPaused(false); isPausedRef.current = false; }} /> 
+					: <PauseButton onClick={() => { setIsPaused(true); isPausedRef.current = true; }} />}
 			</div>
 			<div style={{marginTop:'20px'}}>
 				<SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
